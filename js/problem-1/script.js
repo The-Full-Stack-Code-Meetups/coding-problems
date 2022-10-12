9f4e618375e30b17b50c1f43ce11b986bbd1dfa2
vladbv <vladimirbhelp@gmail.com>
2022-10-12 19:37:06 +0300

    Added Problem 1

--- js/problem-1/script.js
+++ js/problem-1/script.js
@@ -1,9 +1,4 @@
-# coding-problems
-
-In this section, you will find coding problems and their solutions, worked collaboratively by some of our members.
-
-<h2>Problem 1</h2>
-Difficulty: Easy
+ /* Problem 1 
 This problem was asked by Twitter.
 
 You run an e-commerce website and want to record the last N order ids in a log. Implement a data structure to accomplish this, with the following API:
@@ -11,5 +6,33 @@ You run an e-commerce website and want to record the last N order ids in a log. 
 record(order_id): adds the order_id to the log
 get_last(i): gets the ith last element from the log. i is guaranteed to be smaller than or equal to N. You should be as efficient with time and space as possible.
 You should be as efficient with time and space as possible.
+*/
 
-Solution for JS: [Solution](https://github.com/The-Full-Stack-Code-Meetups/coding-problems/js/problem-1/script.js)
+/* Defining data structure with methods */
+
+class Logger {
+/* @param { number } size
+*/
+constructor(size){
+	this.size = size
+	this.log = [];
+	this.currI = 0;
+}
+
+/*
+ * @param {number} order_id
+ */
+record(order_id){
+	this.log[currI] = order_id
+	this.currI = (this.currI + 1) % this.size
+}
+
+/* 
+ * @param {number} i
+ * @return {number}
+ */
+get_last(i){
+	return this.log[(this.currI - i + this.size) % this.size]
+}
+
+}
