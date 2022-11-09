b9d285adad75ef73e25223adde2273c29d76f1db
Gabriela H <67191060+starcluster69@users.noreply.github.com>
2022-11-09 19:46:32 +0200

    Update script.py

--- python/problem-7/script.py
+++ python/problem-7/script.py
@@ -1,3 +1,10 @@
+"""This problem was asked by Google.
+Given a stack of N elements, interleave the first half of the stack with the second half reversed using only one other queue. This should be done in-place.
+Recall that you can only push(item) or pop() from a stack, and enqueue(item) or dequeue() from a queue.
+For example, if the stack is [1, 2, 3, 4, 5], it should become [1, 5, 2, 4, 3]. If the stack is [1, 2, 3, 4], it should become [1, 4, 2, 3].
+Hint: Try working backwards from the end state."""
+
+
 from collections import deque
 
 
