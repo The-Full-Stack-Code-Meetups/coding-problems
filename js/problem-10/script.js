e322dc1ef745974eb1cf283590e4cd4af0561c5d
Demetrios Vlassis <95277454+TheHero9@users.noreply.github.com>
2022-11-23 19:09:32 +0200

    problem 10

--- js/problem-10/script.js
+++ js/problem-10/script.js
@@ -1,23 +1,30 @@
-/* Problem 10 
-This problem was asked by Palantir.
+// This problem was asked by Palantir.
+// Write a program that checks whether an integer is a palindrome.
+// For example, 121 is a palindrome, as well as 888. 678 is not a palindrome.
+// Do not convert the integer into a string.
 
-Write a program that checks whether an integer is a palindrome. For example, 121 is a palindrome, as well as 888. 678 is not a palindrome. Do not convert the integer into a string.
-*/
 
-function isPalindrome(num){
-
-let rem;
-const temp = num;
-let final = 0;
-
-while(num > 0) {
-	rem = num % 10;
-  num = Number.parseInt(num / 10, 10);
-  final = final * 10 + rem;
-}
-
-return final === temp;
-
-}
-
-console.log(isPalindrome(121))
+function palindromeNumber(number){
+    let initialNumber = number
+    var digit, reversed = 0
+  
+      while( number ){
+          //Get right-most digit. Ex. 123/10 → 12.3 → 3
+          digit = number % 10
+  
+          //Add the "digit" to the reversed number
+          reversed = (reversed * 10) + digit
+  
+          //Remove the right-most digit from the starting number
+          number = number/10 | 0
+      }  
+  
+      const answer = (initialNumber === reversed) ? "Palindrome" : "Not a palindrome"
+      return answer
+  }
+  
+  
+  console.log(palindromeNumber(89798)) //Palindrome 
+  console.log(palindromeNumber(781)) //Not a palindrome
+  console.log(palindromeNumber(121)) //Palindrome
+  console.log(palindromeNumber(678)) //Not a palindrome
