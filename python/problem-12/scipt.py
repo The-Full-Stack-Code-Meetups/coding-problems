2a80a4e0665baf87f2fc9c02fdc8172b08de2609
starcluster69 <starcluster333@gmail.com>
2022-11-30 19:07:37 +0200

    Added Problem 12

---
+++ python/problem-12/scipt.py
@@ -0,0 +1,8 @@
+def first_recurring(string):
+    for i in range(0, len(string)-1):
+        curr_element = string[i]
+        next_element = string[i+1]
+        if curr_element == next_element:
+            return curr_element
+    else:
+        return None
