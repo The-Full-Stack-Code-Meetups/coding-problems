5eb9bfdc6f64056571a75af4d81f158d7fd3ebd3
Boris Dundakov <71731579+BorisDundakov@users.noreply.github.com>
2022-12-14 19:53:48 +0200

    Create script.py

---
+++ python/problem-17/script.py
@@ -0,0 +1,20 @@
+"""This problem was asked by Google.
+
+Given the head of a singly linked list, swap every two nodes and return its head.
+
+For example, given 1 -> 2 -> 3 -> 4, return 2 -> 1 -> 4 -> 3."""
+
+
+def node_list(arr):
+    start = 0
+    end = len(arr)
+
+    curr_idx = start
+    while curr_idx < end - 1:
+        arr[curr_idx], arr[curr_idx + 1] = arr[curr_idx + 1], arr[curr_idx]
+        curr_idx += 2
+
+    print(arr)
+
+
+node_list([1, 2, 3, 4, 5])
