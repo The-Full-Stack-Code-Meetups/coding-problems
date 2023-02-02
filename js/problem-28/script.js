1b8a5e2b998996497db9602060877c3e329e36d9
Demetrios Vlassis <95277454+TheHero9@users.noreply.github.com>
2023-02-02 17:09:27 +0200

    problem 28

--- js/problem-28/script.js
+++ js/problem-28/script.js
@@ -1,45 +1,37 @@
-function findNextHigherN(num) {
+// Solution 1
+const MissingNumber = function(array){
+  let result =[]
 
-    //Permutations array
-    let permutations = [];
-  
-    //Used for the final answer
-    const numStart = num;
-  
-    //Split the number
-    let numSplit = num.toString().split("");
-  
-    
-    function backtrack(first = 0) {
-      if (first === numSplit.length) {
-        permutations.push(numSplit.join(""));
-        return;
-      }
-  
-      for (let i = first; i < numSplit.length; i++) {
-        [numSplit[first], numSplit[i]] = [numSplit[i], numSplit[first]];
-        backtrack(first + 1);
-        [numSplit[first], numSplit[i]] = [numSplit[i], numSplit[first]];
-      }
+  // Remove all integers less than 1 and sort the array
+  let filteredSorted = array.filter(e => e>0).sort((a,b) => a-b)
+
+  filteredSorted.reduce((acc, element) =>{
+    // console.log(`${acc} ${element}`)
+
+  // If the accumulator is different than the element => push into results
+    if(acc !== element){
+      result.push(acc)
     }
-  
-    //Get all the permutations in 
-    backtrack();
-  
-  
-    //      const numStart = num;    ↓ 
-    let startingNumber = String(numStart);
-    
-    //Sort
-    let sorted = permutations.sort();
-    //Get Position
-    let positionOfAnswer = (sorted.indexOf(startingNumber))+1;
-   
-    //Result
-    let result = sorted[positionOfAnswer];
-    return result;
-  }
-  
-  console.log(findNextHigherN(1324));
+    return acc+=1
+  }, 1)
+
+  // Make sure we have a missing number into the array and return the first result
+  if(result[0]){
+    return result[0]
+  }   return "No missing numbers in your array"
+}
+
+// Solution 2
+function MissingNumber(arr){
+  let newSet = new Set(arr);
+let num = 1;
+while(newSet.has(num)){
+num++
+}
+return num;
+}
 
 
+console.log(MissingNumber([-1,5,1,2,5,6,7,8])) //3
+console.log(MissingNumber([-10,-10, 8,90,6,7,8])) //1
+console.log(MissingNumber([0,0,0,1,2,3,7,6,5,4])) //7
