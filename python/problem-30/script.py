5801b2a34db2b6805dabbf5869b57bb286efc8be
Boris Dundakov <71731579+BorisDundakov@users.noreply.github.com>
2023-03-08 21:35:36 +0200

    Update script.py

--- python/problem-30/script.py
+++ python/problem-30/script.py
@@ -1,4 +1,5 @@
 ''''
+
 This problem was asked by Twitter.
 
 A permutation can be specified by an array P, where P[i] represents the location of the element at i in the permutation.
@@ -6,6 +7,7 @@ A permutation can be specified by an array P, where P[i] represents the location
 
 Given an array and a permutation, apply the permutation to the array. For example, given the array ["a", "b", "c"]
  and the permutation [2, 1, 0], return ["c", "b", "a"].
+
 '''
 
 
